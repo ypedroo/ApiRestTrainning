5472aad4ab9a7872e596c3eb292115b1c905e561
ypedro <ynoa.pedro@outlook.com>
2019-06-23 22:43:50 -0300

    Add person service interface

---
+++ api-rest-training/api-rest-training/Services/Interfaces/IPersonServices.cs
@@ -0,0 +1,18 @@
+﻿using Accord.Statistics.Kernels;
+using api_rest_training.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_rest_training.Services.Interfaces
+{
+    interface IPersonServices
+    {
+        Person Create(Person person);
+        Person FindViewById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(Person person);
+    }
+}
