c3e658a863b3b4a1b7539f345e9cd9222ebf374d
ypedro <ynoa.pedro@outlook.com>
2019-07-02 12:56:58 -0300

    Change DI life cycle

--- api-rest-training/api-rest-training/Startup.cs
+++ api-rest-training/api-rest-training/Startup.cs
@@ -28,7 +28,7 @@ namespace api_rest_training
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddScoped<ICalculatorRepository, CalculatorRepository>();
+            services.AddTransient<ICalculatorRepository, CalculatorRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
