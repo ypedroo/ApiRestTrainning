21b069b506db287446b0310f4048e39b5abbf3f6
ypedro <ynoamota@pmenos.com.br>
2019-07-08 12:07:02 -0300

    Adding get and post to the controller

--- api-rest-training/api-rest-training/Controllers/PersonsController.cs
+++ api-rest-training/api-rest-training/Controllers/PersonsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api_rest_training.Models;
 using api_rest_training.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,35 +13,39 @@ namespace api_rest_training.Controllers
     public class PersonsController : ControllerBase
     {
         //Instaciate DI obj
-        private IPersonServices _personService;
+        private readonly IPersonServices personService;
 
         public PersonsController(IPersonServices personServices)
         {
-            _personService = personServices;
+            this.personService = personServices;
         }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(personService.FindAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var person = personService.FindViewById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Person person)
         {
+            if (person == null) return NotFound();
+            return new ObjectResult(personService.Create(person));
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] string value)
         {
         }
 
