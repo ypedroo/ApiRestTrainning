ecee1f82d9915f39c0e5e47a372b7e2bcc731b16
ypedro <ynoa.pedro@outlook.com>
2019-07-23 13:29:10 -0300

    Add app con db context

--- api-rest-training/api-rest-training/Models/Context/MySQLContext.cs
+++ api-rest-training/api-rest-training/Models/Context/MySQLContext.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace api_rest_training.Models.Context
 {
-    public class MySQLContext
+    public class MySQLContext : DbContext
     {
+        public MySQLContext ()
+        {
+
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
     }
 }
