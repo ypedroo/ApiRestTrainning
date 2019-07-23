bbb8e11bf8bf03e681ff06f7c448aa61c33244ba
ypedro <ynoa.pedro@outlook.com>
2019-07-23 12:51:17 -0300

    Adding app db context

---
+++ api-rest-training/api-rest-training/Models/Context/MySQLContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_rest_training.Models.Context
+{
+    public class MySQLContext
+    {
+    }
+}
